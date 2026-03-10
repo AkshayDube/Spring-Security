940ab9391c7c8cbd4a54440c7a6602fd9ebe0cfa
akshay.dubey <dbyakshay@gmail.com>
2026-03-10 19:37:14 +0530

    Spring Security with default log password.

---
+++ security-codebase/src/main/java/com/akdubey/security_codebase/SecurityCodebaseApplication.java
@@ -0,0 +1,13 @@
+package com.akdubey.security_codebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityCodebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityCodebaseApplication.class, args);
+	}
+
+}
---
+++ security-codebase/src/test/java/com/akdubey/security_codebase/SecurityCodebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akdubey.security_codebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityCodebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
